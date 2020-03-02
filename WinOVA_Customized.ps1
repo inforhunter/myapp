eb0a5a2541f1396beaea45f0ed259145f1d554b6
inforhunter <li.jun@flow-ever.com>
2020-03-02 16:56:21 +0800

    Add files via upload

---
+++ WinOVA_Customized.ps1
@@ -0,0 +1,57 @@
+$customizationRanFile = [System.Environment]::GetEnvironmentVariable('TEMP','Machine') + "\ran_customization"
+$customizationLogFile = [System.Environment]::GetEnvironmentVariable('TEMP','Machine') + "\customization-log.txt"
+
+if(! (Test-Path -LiteralPath $customizationRanFile)) {
+    "Customization Started @ $(Get-Date)" | Out-File -FilePath $customizationLogFile
+
+    $EthernetInterfaceAliasName = "Ethernet0"
+    $VMwareToolsExe = "C:\Program Files\VMware\VMware Tools\vmtoolsd.exe"
+
+    [xml]$ovfEnv = & $VMwareToolsExe --cmd "info-get guestinfo.ovfEnv" | Out-String
+    $ovfProperties = $ovfEnv.ChildNodes.NextSibling.PropertySection.Property
+
+    $ovfPropertyValues = @{}
+    foreach ($ovfProperty in $ovfProperties) {
+        $ovfPropertyValues[$ovfProperty.key] = $ovfProperty.Value
+    }
+
+    # uncomment below for debugging which will output all OVF Properties to logfile
+    # $ovfProperties | Out-File -FilePath $customizationLogFile -Append
+
+    # Configure Static IP
+    if($ovfPropertyValues['hostname'] -ne "") {
+
+        # Rename Computer & Description to match Hostname
+        "Renaming Computer Name and Description to $($ovfPropertyValues['hostname'])" | Out-File -FilePath $customizationLogFile -Append
+        Rename-Computer -NewName $ovfPropertyValues['hostname'] | Out-File -FilePath $customizationLogFile -Append
+        Get-CimInstance -ClassName Win32_OperatingSystem | Set-CimInstance -Property @{Description = $ovfPropertyValues['hostname']} | Out-File -FilePath $customizationLogFile -Append
+
+        # Configure Networking
+        "Configuring IP Address to $($ovfPropertyValues['ipaddress'])" | Out-File -FilePath $customizationLogFile -Append
+        "Configuring Netmask to $($ovfPropertyValues['netmask'])" | Out-File -FilePath $customizationLogFile -Append
+        "Configuring Gateway to $($ovfPropertyValues['gateway'])" | Out-File -FilePath $customizationLogFile -Append
+        New-NetIPAddress –InterfaceAlias $EthernetInterfaceAliasName -AddressFamily IPv4 –IPAddress $ovfPropertyValues['ipaddress'] –PrefixLength $ovfPropertyValues['netmask'] | Out-File -FilePath $customizationLogFile -Append
+        New-NetRoute -DestinationPrefix 0.0.0.0/0 -InterfaceAlias $EthernetInterfaceAliasName -NextHop $ovfPropertyValues['gateway'] | Out-File -FilePath $customizationLogFile -Append
+
+        # Configure DNS
+        "Configuring DNS to $($ovfPropertyValues['dns'])" | Out-File -FilePath $customizationLogFile -Append
+        Set-DnsClientServerAddress -InterfaceAlias $EthernetInterfaceAliasName -ServerAddresses $ovfPropertyValues['dns'] | Out-File -FilePath $customizationLogFile -Append
+        # Sleep to ensure DNS changes go into effect for AD Join
+        Start-Sleep -Seconds 5
+
+        # Configure Active Directory
+        if($ovfPropertyValues['ad_domain'] -ne "" -and $ovfPropertyValues['ad_username'] -ne "" -and $ovfPropertyValues['ad_password'] -ne "") {
+            $joinCred = New-Object pscredential -ArgumentList ([pscustomobject]@{
+                UserName = $ovfPropertyValues['ad_username'] + "@" + $ovfPropertyValues['ad_domain']
+                Password = (ConvertTo-SecureString -String $ovfPropertyValues['ad_password'] -AsPlainText -Force)[0]
+            })
+            "Joining Active Directory Domain $($ovfPropertyValues['ad_domain'])" | Out-File -FilePath $customizationLogFile -Append
+            Add-Computer -NewName $ovfPropertyValues['hostname'] -Domain $ovfPropertyValues['ad_domain'] -Credential $joinCred -Restart | Out-File -FilePath $customizationLogFile -Append
+        }
+    } else {
+        "No OVF Properties were found, defaulting to DHCP for networking" | Out-File -FilePath $customizationLogFile -Append
+    }
+
+    # Create ran file to ensure we do not run again
+	Out-File -FilePath $customizationRanFile
+}
